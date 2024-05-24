1a2ea2eae2874a11c3f981c265443ce0d8018754
Kendall <vedaker@st.ug.edu.gh>
2024-05-24 10:52:14 +0000

    Initial commit

--- App.js
+++ App.js
@@ -1,21 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style= {{backgroundColor: "teal", flex: 1, padding: 60}}>
+        <Text style= {{ fontSize: 24 }}>
+          My name is<Text style = {{ fontWeight: "bold"}}> Vanessa Daker. 
+          </Text>
+        </Text>
     </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+)};
